5f6625199a6059ba17ef98831e78bedf5f33ecde
Seth Walsh <seth.walsh@utah.edu>
2014-04-04 13:51:44 -0600

    Windows Service & Setup project added and tested

---
+++ windows_client/MonitoringClientService/MonitoringClientService/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace MonitoringClientService
+{
+    public partial class MonitoringClientService : ServiceBase
+    {
+        Process client;
+
+        public MonitoringClientService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Start Monitoring-Client
+           client =  Process.Start("C:\\Tools\\Monitoring\\LMWinClient4.exe");
+        }
+
+        protected override void OnStop()
+        {
+            client.Kill();
+        }
+    }
+}
