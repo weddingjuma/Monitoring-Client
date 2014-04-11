cd6eafd8a98a58940ac452cc8acb7b210681efc6
Seth Walsh <seth.walsh@utah.edu>
2014-04-11 13:40:23 -0600

    linux installer folder for the new full installation process on a linux system

---
+++ linux_install/linux_reader/linux.cpp
@@ -0,0 +1,68 @@
+#include "linux.h"
+
+/*
+    A fairly simple way of checking if the given file descriptor is valid.  Used to check if for some reason either the Client or System closed
+    our FIFO for some reason, then we can reopen it for reading.
+*/
+int is_valid_fd(int fd)
+{
+    return fcntl(fd, F_GETFL) != -1 || errno != EBADF;
+}
+
+/*
+    Entry point pthread that will display a message to the user if they're logged into a graphical desktop.  It first checks to see if they're running
+    KDE and uses kdialog.  Otherwise it defaults to X native message system xmessage.
+*/
+void *display_msg(void *msg)
+{
+    char *message = (char*)msg;
+    char x[200];
+    vector<string> olines;
+	FILE *f;
+    f = popen("pidof ksmserver", "r");
+	while(fgets(x, 200, f) != NULL)
+    {
+        olines.push_back(x);
+    }
+
+    if(olines.size() > 0)
+    {
+        // kdialog
+        string cmd = "kdialog --title \"Account Expiration Warning\" --warningcontinuecancel \"";
+        cmd += message;
+        cmd += "\"";
+        system(cmd.c_str());
+    }
+    else
+    {
+        // xmessage
+        string cmd = "xmessage -title \"Account Expiration Warning\" -buttons ok -center ";
+        cmd += message;
+        system(cmd.c_str());
+    }
+}
+
+/*
+    Checks to make sure the machine is logged in.  Because this program belongs in Autostart, this part shouldn't be needed, but just in case
+    this will ensure that the program exits when the user logs out and doesn't stay running.
+*/
+bool is_logged_in()
+{
+    char x[200];
+    vector<string> olines;
+	FILE *f;
+    f = popen("who", "r");
+
+	while(fgets(x, 200, f) != NULL)
+    {
+        if(string(x).find("(:0)") != string::npos)
+        {
+            pclose(f);
+            return true;
+        }
+    }
+
+    pclose(f);
+    return false;
+}
+
---
+++ linux_install/lxinstall.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+ 
+export TEMPSELECT=/tmp/choice
+export OUTPUT=/tmp/out
+
+function default_server {
+	# clear terminal
+	clear
+	
+	# create directories
+	mkdir /opt/monitoring
+	mkdir /opt/monitoring/config
+	mkdir /opt/monitoring/data
+	mkdir /var/log/monitoring
+	
+	# copy boost files
+	cp boost/ /usr/include/ -r
+	cp threadpool.hpp /usr/include/boost/
+	cp threadpool/ /usr/include/boost/ -r
+	cp mysql++/ /usr/include/ -r
+	
+	# compile
+	g++ -o monitoring-server main.cpp -lpthread -lboost_system -lboost_regex -lboost_thread -lmysqlpp -I/usr/include/mysql
+	
+	# copy server files
+	cp monitoring-server /opt/monitoring/
+	cp default.cfg /opt/monitoring/config
+	cp masterlist.txt /opt/monitoring/config
+	cp mserver /etc/init.d/
+	
+	# build database TODO:::
+	dialog --inputbox "Enter address of your MySQL server:" 30 80 2>$TEMPSELECT
+	OUT=`cat $TEMPSELECT`
+	dialog --inputbox "Enter the username with database create rights on your MySQL server:" 30 80 2>$TEMPSELECT
+	USER=`cat $TEMPSELECT`
+	dialog --inputbox "Enter the password for your MySQL user:" 30 80 2>$TEMPSELECT
+	PASS=`cat $TEMPSELECT`
+	dialog --inputbox "Enter a name for your database:" 30 80 2>$TEMPSELECT
+	NAME=`cat $TEMPSELECT`
+	mysqladmin -h \'$OUT\' create $NAME -u $USER -p $PASS
+	gunzip < monitoring-server-db.gz | mysql $NAME
+	
+	# inform user they need to manually add init script to default runlevel based on their distro method
+	dialog --msgbox \
+		"Installation has completed.  A manual add of the init script to default runlevel is required in order for the server to perform as	a service\n\nYou should also edit the server configuration file to suit your needs located in /opt/monitoring/config/default.cfg" 30 80
+}
+
+function custom_server {
+	clear
+	dialog --inputbox "Type the location you want the Server installed to:" 18 60 2>$TEMPSELECT
+	OUT=`cat $TEMPSELECT`
+	mkdir $OUT
+	mkdir $OUT/monitoring
+	mkdir $OUT/monitoring/config
+	mkdir $OUT/monitoring/data
+	mkdir /var/log/monitoring	
+}
+
+# Check that the installation is being performed with root privleges otherwise directory creation, which is required,
+# will fail.
+if [[ $EUID -ne 0 ]]; then
+	echo "This installation must be run as root" 1>&2
+	exit 1
+fi
+
+# Ask if Server or Client install
+dialog --clear --msgbox \
+	"Welcome to the Monitoring-Client Linux Server or Client install.  Custom installations will allow you to choose your own installation directories.  If you choose a default install the directory structure will be created for you.\n\nIn the case of a Server installation a MySQL database will be created for you on the server address you enter. In which case a valid username and password for either remote or local access (and the necessary privleges) depending on if you are installing to a remote server is required.\n\nMySQL is required for Monitoring-Client Server to run, make sure it is installed.  Boost 1.55.0 libraries and MySQL++ will be installed on this system.\n\nPress ENTER to start installation.\n" 30 80
+	
+dialog --clear --menu "Monitoring-Client Linux Installation Main Menu" \
+		30 80 5 \
+		defaultserver "Default Server Installation" \
+		customserver "Custom Server Installation" \
+		defaultclient "Default Client Installation" \
+		customclient "Custom Client Installation" \
+		quit "Quit" 2>$TEMPSELECT
+SELECTION=`cat $TEMPSELECT`
+
+case $SELECTION
+in
+defaultserver ) default_server; ;;
+customserver ) custom_server; ;;
+defaultclient ) ;;
+customclient ) ;;
+quit ) clear; exit ;;
+esac
+clear
+echo $SELECTION
