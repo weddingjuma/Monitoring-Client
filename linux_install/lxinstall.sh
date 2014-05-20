8ac3dcf43d91f0b96290052b398e9aab46452f8b
Seth Walsh <seth.walsh@utah.edu>
2014-05-20 13:12:15 -0600

    more changes to installer

--- linux_install/lxinstall.sh
+++ linux_install/lxinstall.sh
@@ -47,6 +47,8 @@ function default_server {
 
 function custom_server {
 	clear
+	
+	# Create directories
 	dialog --inputbox "Type the location you want the Server installed to:" 18 60 2>$TEMPSELECT
 	OUT=`cat $TEMPSELECT`
 	mkdir $OUT
@@ -54,6 +56,87 @@ function custom_server {
 	mkdir $OUT/monitoring/config
 	mkdir $OUT/monitoring/data
 	mkdir /var/log/monitoring	
+	
+	# Copy Boost and Mysql++ files
+	cp boost/ /usr/include/ -r
+	cp threadpool.hpp /usr/include/boost/
+	cp threadpool/ /usr/include/boost/ -r
+	cp mysql++/ /usr/include/ -r
+	
+	# Compile 
+	g++ -o monitoring-server main.cpp -lpthread -lboost_system -lboost_regex -lboost_thread -lmysqlpp -I/usr/include/mysql
+	
+	# Copy server files
+	cp monitoring-server $OUT/monitoring/
+	cp default.cfg $OUT/monitoring/config/
+	cp masterlist.txt $OUT/monitoring/config/
+	cp mserver /etc/init.d/
+	
+	# Build database TODO:::
+	
+	# Inform the user they need to manually add the init script to their default runlevel based on their distro installation method
+	dialog --msgbox \
+		"Installation has complted.  A manual add of the init script to default runlevel is required in order for the server to perform as a service.\n\nYou should also edit the server configuration file to suit your needs located in $OUT/monitoring/config/default.cfg" 30 80
+}
+
+function default_client {
+	clear
+	
+	# Create directories
+	mkdir /opt/monitoring
+	mkdir /opt/monitoring/config
+	mkdir /opt/monitoring/data
+	mkdir /var/log/monitoring/
+	
+	# Copy Boost files
+	cp boost/ /usr/include/ -r
+	cp threadpool.hpp /usr/include/boost/
+	cp threadpool/ /usr/include/boost/ -r
+	
+	# Compile
+	g++ -o monitoring-client main.cpp -lpthread -lboost_system -lboost_regex -lboost_thread -lboost_threadpool
+	
+	# Copy client files
+	cp monitoring-client /opt/monitoring/
+	cp default.cfg /opt/monitoring/config/
+	cp masterlist.txt /opt/monitoring/config/
+	cp mclient /etc/init.d/
+	
+	# Compile reader
+	g++  -o mclient_reader main.cpp linux.cpp -lpthread
+	
+	# Copy reader
+	cp mclient_reader /opt/monitoring/
+	cp mclient_reader.desktop 
+	
+	dialog --msgbox \
+		"Installation has completed.  A manual add of the init script to default runlevel is required in order for the client to perform as a service.\n\nYou should also edit the client configuration file to suit your needs located in /opt/monitoring/config/default.cfg" 30 80
+	
+}
+
+function custom_client {
+	clear
+	
+	# Create custom directories
+	dialog --inputbox "Type the location you want the Client installed to:" 18 60 2>$TEMPSELECT
+	OUT=`cat $TEMPSELECT`
+	mkdir $OUT
+	mkdir $OUT/monitoring
+	mkdir $OUT/monitoring/config
+	mkdir $OUT/monitoring/data
+	mkdir /var/log/monitoring/
+	
+	# Copy Boost files
+	cp boost/ /usr/include/ -r
+	cp threadpool.hpp /usr/include/boost/
+	cp threadpool/ /usr/include/boost/ -r
+
+	# Compile
+	g++ -o monitoring-client main.cpp -lpthread -lboost_system -lboost_regex -lboost_thread -lboost_threadpool
+	
+	# Copy client files
+	
+	
 }
 
 # Check that the installation is being performed with root privleges otherwise directory creation, which is required,
